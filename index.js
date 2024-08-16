9a1bf2df00bb49442ff3cf4daf5f6205a53a1df5
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-16 14:58:50 -0700

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,27 @@
+// Chapter-33:specifing-date-and-time-js
+
+// var hello = new Date("june 30, 2035");
+// console.log(hello);
+
+// Invalid Date 
+// var hello = new Date("june 32, 2035");
+// console.log(hello);
+
+// real life example finding remaining days in election
+// Days in electiion
+// const now = new Date();
+// const election = new Date("august 20, 2024");
+// const remainingDaysINMiliseconds = election - now;
+// console.log(remainingDaysINMiliseconds);
+// const daysTOElection = remainingDaysINMiliseconds / (1000 * 60 * 60 * 24);
+// console.log(daysTOElection);
+// console.log(Math.floor(daysTOElection));
+
+// hours in electiion
+// const now = new Date();
+// const election = new Date("august 20, 2024");
+// const remainingHoursINMiliseconds = election - now;
+// console.log(remainingHoursINMiliseconds);
+// const HoursTOElection = remainingHoursINMiliseconds / (1000 * 60 * 60 );
+// console.log(HoursTOElection);
+// console.log(Math.floor(HoursTOElection));
